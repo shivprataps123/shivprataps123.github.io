83c922e5a8100751f397ef79650ade616ce52d35
Shiv Pratap Singh Yadav <shivpratapsinghyadav38@gmail.com>
2022-12-05 16:53:25 +0530

    Updates

---
+++ static/js/219.5431fd0f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_portfolio=self.webpackChunkmy_portfolio||[]).push([[219],{219:function(o,e,f){f.r(e),e.default={}}}]);
+//# sourceMappingURL=219.5431fd0f.chunk.js.map
---
+++ static/js/591.5cac5a67.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_portfolio=self.webpackChunkmy_portfolio||[]).push([[591],{591:function(o,e,f){f.r(e),e.default={}}}]);
+//# sourceMappingURL=591.5cac5a67.chunk.js.map
