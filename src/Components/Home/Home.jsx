3dc7a47c7270db38293ebaa65a0124a7f1c55583
Shiv Pratap Singh Yadav <shivpratapsinghyadav38@gmail.com>
2023-03-11 14:40:28 +0530

    skills

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,10 +2,10 @@
 import Typewriter from 'typewriter-effect'
 import React from "react";
 import {BiDownload} from 'react-icons/bi'
-import pdf from '../../fp06_006-Shiv_Pratap_Singh_Yadav_Resume.pdf'
+import pdfs from '../../fp06_006-Shiv_Pratap_Singh_Yadav_Resume.pdf'
 //import ReactDOM from 'react-dom/client';
 import "./Home.css";
-
+let pdf="https://drive.google.com/uc?export=download&id=1FDi6FTc3jF13LVBwpJlRNpdCgthyZ53k"
 
 const Home=()=>{
   return (
@@ -30,7 +30,7 @@ const Home=()=>{
           </h1>
         <a
           href={pdf}
-          target="_blank"
+          target=""
           rel="noreferrer"
           download
         >
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,8 @@ import Link from "react-scroll/modules/components/Link";
 import React from "react";
 import Hamburger from  'hamburger-react';
 import "./Navbar.css";
-import pdf from '../../fp06_006-Shiv_Pratap_Singh_Yadav_Resume.pdf'
+import pdfs from '../../fp06_006-Shiv_Pratap_Singh_Yadav_Resume.pdf'
+let pdf="https://drive.google.com/uc?export=download&id=1FDi6FTc3jF13LVBwpJlRNpdCgthyZ53k"
 export default function Navbar() {
   return (
     <div className="navbar">
